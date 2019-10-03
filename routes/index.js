9d238fa7afd4043f45336231cba9d4ecd09f631d
divya <divya.dp35@gmail.com>
2019-10-03 02:50:04 -0700

    Div

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ router.get('/db', async (req, res) => {
     const client = await pool.connect()
     const result = await client.query('SELECT * FROM test_table');
     const results = { 'results': (result) ? result.rows : null};
-    res.render('/index', results );
+    res.render('pages/db', results );
     client.release();
   } catch (err) {
     console.error(err);
