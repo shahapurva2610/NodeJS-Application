81cb7cc59490a710c1b3b01324b86026dc4a6d8e
divya <divya.dp35@gmail.com>
2019-10-03 01:13:08 -0700

    Div

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+var pg = require('pg');
+router.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+      { console.error(err); response.send("Error " + err); }
+      else
+      { response.render('/db', {results: result.rows} ); }
+    });
+  });
+});
+module.exports = router;
