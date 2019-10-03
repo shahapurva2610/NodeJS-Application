1eac55ff8e5e3296d8053e1c536f1b0c6634cc95
divya <divya.dp35@gmail.com>
2019-10-03 01:53:09 -0700

    Div

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 var pg = require('pg');
-router.get('/db', function (request, response) {
+router.get('/index', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT * FROM test_table', function(err, result) {
       done();
